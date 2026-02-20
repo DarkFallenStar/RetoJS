140bf2cb9511adfcc76617c4f1ff9309126f0f5f
John Doe <Juanmacastro1000@gmail.com>
2026-02-19 22:41:25 -0500

    Carrito en proceso no usar :D

--- mainPageScript.js
+++ mainPageScript.js
@@ -1,6 +1,6 @@
 //Ponemos esto pa que se ejecute despues de la carga del HTML, para que asi cargue todo fino
 
-function Producto(id,nombre, stock, precio, imagen) {
+function Producto(id, nombre, stock, precio, imagen) {
     this.id = id;
     this.nombre = nombre;
     this.stock = stock;
@@ -8,21 +8,24 @@ function Producto(id,nombre, stock, precio, imagen) {
     this.imagen = imagen;
 }
 
-const papel= new Producto("papel", "Papel resma", 10, 16000, "https://tauro.com.co/wp-content/uploads/2020/02/Papel-resma-Marfil-430x490.jpg");
-const tijera= new Producto("Tijeras", "Tijeras", 20, 6100, "https://officemax.vtexassets.com/arquivos/ids/1347697/35697_1.jpg?v=638158814488200000");
-const legajador= new Producto("Legajador carta", "Legajador carta", 5, 7750,"https://tauro.com.co/wp-content/uploads/2019/12/83525-430x490.png");
-const lapicero= new Producto("lapicero", "lapicero", 99, 700,"https://tauro.com.co/wp-content/uploads/2019/12/11635.png");
-const Cinta= new Producto("Cinta", "Cinta", 32, 5700,"https://tauro.com.co/wp-content/uploads/2019/12/131723-430x490.png");
-const Colores= new Producto("Colores", "Colores", 18, 1200,"https://tauro.com.co/wp-content/uploads/2019/12/139867-300x300.png");
-const Bond= new Producto("Bond", "Papel bond", 10, 2400,"https://tauro.com.co/wp-content/uploads/2020/05/PAPEL-BOND.jpg");
-const Borrador= new Producto("Borrador", "Borrador", 210, 550,"https://tauro.com.co/wp-content/uploads/2021/02/10502-430x490.jpg");
-const Cuaderno= new Producto("Cuaderno", "Cuaderno", 22, 700,"https://tauro.com.co/wp-content/uploads/2025/01/CUADERNO-COSIDO-DOBLE-RAYADO-SURT-FAMA-430x490.jpg");
-const Grapas= new Producto("Grapas", "Grapas", 4, 4050,"https://tauro.com.co/wp-content/uploads/2019/12/11484.png");
-const Temperas =new Producto("Temperas", "Temperas", 8, 2300,"https://tauro.com.co/wp-content/uploads/2025/01/Tempera-Marfil-6-colores-430x490.jpg");
-const Sacapuntas =new Producto("Sacapuntas", "Sacapuntas", 50, 1500,"https://tauro.com.co/wp-content/uploads/2022/01/36733-430x490.jpg");
-const Productos = [papel, tijera, legajador, lapicero,Cinta,Colores,Bond,Borrador,Cuaderno, Grapas, Temperas, Sacapuntas];
+const papel = new Producto("papel", "Papel resma", 10, 16000, "https://tauro.com.co/wp-content/uploads/2020/02/Papel-resma-Marfil-430x490.jpg");
+const tijera = new Producto("Tijeras", "Tijeras", 20, 6100, "https://officemax.vtexassets.com/arquivos/ids/1347697/35697_1.jpg?v=638158814488200000");
+const legajador = new Producto("Legajador carta", "Legajador carta", 5, 7750, "https://tauro.com.co/wp-content/uploads/2019/12/83525-430x490.png");
+const lapicero = new Producto("lapicero", "lapicero", 99, 700, "https://tauro.com.co/wp-content/uploads/2019/12/11635.png");
+const Cinta = new Producto("Cinta", "Cinta", 32, 5700, "https://tauro.com.co/wp-content/uploads/2019/12/131723-430x490.png");
+const Colores = new Producto("Colores", "Colores", 18, 1200, "https://tauro.com.co/wp-content/uploads/2019/12/139867-300x300.png");
+const Bond = new Producto("Bond", "Papel bond", 10, 2400, "https://tauro.com.co/wp-content/uploads/2020/05/PAPEL-BOND.jpg");
+const Borrador = new Producto("Borrador", "Borrador", 210, 550, "https://tauro.com.co/wp-content/uploads/2021/02/10502-430x490.jpg");
+const Cuaderno = new Producto("Cuaderno", "Cuaderno", 22, 700, "https://tauro.com.co/wp-content/uploads/2025/01/CUADERNO-COSIDO-DOBLE-RAYADO-SURT-FAMA-430x490.jpg");
+const Grapas = new Producto("Grapas", "Grapas", 4, 4050, "https://tauro.com.co/wp-content/uploads/2019/12/11484.png");
+const Temperas = new Producto("Temperas", "Temperas", 8, 2300, "https://tauro.com.co/wp-content/uploads/2025/01/Tempera-Marfil-6-colores-430x490.jpg");
+const Sacapuntas = new Producto("Sacapuntas", "Sacapuntas", 50, 1500, "https://tauro.com.co/wp-content/uploads/2022/01/36733-430x490.jpg");
+const Productos = [papel, tijera, legajador, lapicero, Cinta, Colores, Bond, Borrador, Cuaderno, Grapas, Temperas, Sacapuntas];
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    let counter = 0
+    const elementosComprados = [];
 
     function crearTarjeta(producto) {
         const contenedor = document.getElementById("contenido");
@@ -37,36 +40,41 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
         contenedor.appendChild(tarjeta)
     }
+
     Productos.forEach(producto => {
         crearTarjeta(producto);
     });
-    
-    const buttons = document.querySelectorAll(".ponerCarro")   
-    
+
+
+    function addCounter(e) {
+
+        const boton = e.target;
+        const idProducto = boton.id
+        const producto = Productos.find(i => i.id === idProducto);
+
+        if (producto.stock > 0) {
+            producto.stock--;
+            document.getElementById(`stock-${producto.id}`).innerHTML = `Stock: ${producto.stock}`;
+
+            counter++
+            document.getElementById("contador").innerHTML = counter;
+        }
+        else {
+            alert("YA NO QUEDA MAS " + idProducto)
+        }
+
+    }
+
+
+
+    const buttons = document.querySelectorAll(".ponerCarro")
+
     buttons.forEach(butt => {
         butt.addEventListener('click', addCounter);
+        butt.addEventListener("")
     });
-    
-    
+
+
 });
-let counter = 0
 
 
-function addCounter(e){
-
-    const boton = e.target;
-    const idProducto = boton.id 
-    const producto = Productos.find(i => i.id === idProducto);
-
-    if (producto.stock > 0){
-        producto.stock--;
-        document.getElementById(`stock-${producto.id}`).innerHTML = `Stock: ${producto.stock}`;
-
-        counter++
-        document.getElementById("contador").innerHTML = counter;
-    }
-    else{
-        alert("YA NO QUEDA MAS")
-    }
-
-}
